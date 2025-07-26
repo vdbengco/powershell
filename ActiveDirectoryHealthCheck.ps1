88a25a1e33d1f31a90d348a3beca5f71d1bc075b
kabang <vdbengco@gmail.com>
2025-07-26 05:33:44 -0400

    Update and rename adhealth.ps1 to ActiveDirectoryHealthCheck.ps1

---
+++ ActiveDirectoryHealthCheck.ps1
@@ -0,0 +1,23 @@
+<#
+  .SYNOPSIS
+  ADHealth.ps1 - Domain Controller Health Check Script.
+  
+  .DESCRIPTION
+  This script performs a list of common health check to a specific domain, or the entire forest.
+
+  .OUTPUTS
+  The results are currently only output HTML report file.
+
+  .PARAMETER DomainName
+  Perform a health check on a specific Active Directory domain.
+
+  .NOTES
+  Written by: VERSON BENGCO
+  Website:
+  LinkedIn:
+  X:
+    
+  .CHANGELOG
+  V0.1.0 Initial Draft
+#>
+
